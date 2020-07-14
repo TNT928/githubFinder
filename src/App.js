a3517fb5655f532f7893ce30172b5b87a9721f61
Michael Vanderloon <jenkins92886@gmail.com>
2020-07-14 00:02:34 -0400

    deploy to netfliy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, {Fragment} from 'react';
-import Users from './users/Users';
+import React from 'react';
+import Home from './Pages/Home';
 import User from './users/user';
-import Search from './users/search';
+import NotFound from './Pages/NotFound';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Alert from './alert';
 import About from './Pages/about';
@@ -11,41 +11,24 @@ import AlertState from './context/alert/Alertstate';
 import './App.css';
 
 const App = () => {
- 
-
-
   return (
     <GithubState>
-    <AlertState>
-       <Router>
-        <div className="App">
-          <Navbar />
-          <div className="container">
-            <Alert />
-            <Switch>
-              <Route
-                exact
-                path="/"
-                render={(props) => (
-                  <Fragment>
-                    <Search />
-                    <Users />
-                  </Fragment>
-                )}
-              />
-              <Route exact path="/about" component={About} />
-              <Route
-                exact
-                path="/user/:login"
-                component= {User}
-                
-              />
-            </Switch>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/user/:login" component={User} />{' '}
+                <Route component={NotFound} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
-    </AlertState>
-     
+        </Router>
+      </AlertState>
     </GithubState>
   );
 };
